1ca8fd43050d4b5236f36d7f3f20128eab1f6d17
Muqadas Fatima <muqadesf532003@gmail.com>
2025-06-04 13:22:49 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+        // let prevbtn = document.querySelector("#customPrevBtn");
+        // let nextbtn = document.querySelector("#customNextBtn");
+
+        // prevbtn.addEventListener("click", function() {
+
+        //     prevbtn.style.color = "white";        
+        //     prevbtn.style.backgroundColor = "rgb(158, 127, 97)"; 
+
+        //     nextbtn.style.color = "rgb(158, 127, 97)";
+        //     nextbtn.style.backgroundColor = "rgb(255, 243, 232)";
+        // });
+
+        // nextbtn.addEventListener("click", function() {
+
+        //     nextbtn.style.color = "white";
+        //     nextbtn.style.backgroundColor = "rgb(158, 127, 97)";
+
+        //     prevbtn.style.color = "rgb(158, 127, 97)";
+        //     prevbtn.style.backgroundColor = "rgb(255, 243, 232)";
+        // });
